17eb85291d125cd11c8e32a229c64b2cbd67f58f
MagomedovMurad20 <magdev09@gmail.com>
2024-05-15 18:47:41 +0300

    прописал роуты для работы со студентами и классами

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\GradeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Группа роутов для студентов
+Route::get('students', [StudentController::class, 'index']);
+Route::post('student/add', [StudentController::class, 'store']);
+Route::get('student/{product}', [StudentController::class, 'show']);
+Route::post('student/{product}', [StudentController::class, 'update']);
+Route::delete('student/{product}', [StudentController::class, 'destroy']);
+
+//Группа роутов для классов
+Route::get('students', [GradeController::class, 'index']);
+Route::post('student/add', [GradeController::class, 'store']);
+Route::get('student/{product}', [GradeController::class, 'show']);
+Route::post('student/{product}', [GradeController::class, 'update']);
+Route::delete('student/{product}', [GradeController::class, 'destroy']);

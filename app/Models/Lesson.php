7ecf2213a19fa2c1798e39f442271b8d80b1b00d
MagomedovMurad20 <magdev09@gmail.com>
2024-05-16 19:33:52 +0300

    fix model Lesson

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -2,15 +2,10 @@
 
 namespace App\Models;
 
-use App\Models\Student;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Lesson extends Model
 {
     use HasFactory;
-    public function student()
-    {
-        return $this->hasMany(Student::class);
-    }
 }

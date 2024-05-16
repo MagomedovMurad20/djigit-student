0bfcc7cdaf35dc206ee76f5cb6acb1299be3f069
MagomedovMurad20 <magdev09@gmail.com>
2024-05-16 19:17:49 +0300

    добавил связи между Студентами, Уроками, Классами

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Grade;
+use App\Models\Lesson;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
     use HasFactory;
+
+    public function grade()
+    {
+        return $this->hasOne(Grade::class);
+    }
+    public function lesson()
+    {
+        return $this->hasMany(Lesson::class);
+    }
 }

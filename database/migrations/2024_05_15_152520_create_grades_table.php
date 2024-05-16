ac7ebe6bc693d5db3ea991f1d091e70dfddc2ccb
MagomedovMurad20 <magdev09@gmail.com>
2024-05-16 19:38:20 +0300

    fix migrations students, grades

--- database/migrations/2024_05_15_152520_create_grades_table.php
+++ database/migrations/2024_05_15_152520_create_grades_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->foreignId('student_id')->nullable()->constrained()->onDelete('set null');
+            $table->integer('student_id')->nullable()->constrained()->onDelete('set null');
         });
     }
 
